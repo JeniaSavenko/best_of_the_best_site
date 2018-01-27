92e64d0c46ed5b5640645029eb4615e40b8d3043
JeniaSavenko <34649508+JeniaSavenko@users.noreply.github.com>
2018-01-27 11:02:11 +0300

    magistr yoba 15

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,27 +2,33 @@ $(document).ready(function () {
   mMenu();
 
 
-
-
   function mMenu() {
-    // if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
       var btnMenu = $(".intro-section__menu-btn");
       var menu = $(".intro-section__item--menu");
       var closeBtn = $(".intro-section__close-btn--hidden-for-pc");
+
+
       btnMenu.on("click", function () {
         menu.show().addClass("collapse");
       });
 
-      if(menu.hasClass("collapse")) {
-        $(".intro-section__logo--default").hide();
-      };
-
+      $(".intro-section__logo--menu").hide();
       menu.hide();
 
-      closeBtn.on("click", function(){
+      if (menu.hasClass("collapse")) {
+        $(".intro-section__logo--menu").show();
+        $(".intro-section__logo--default").hide();
+        menu.hide();
+      }
+      ;
+
+
+
+      closeBtn.on("click", function () {
         menu.hide().removeClass("collapse");
       });
     }
-  // }
+  }
 });
 
