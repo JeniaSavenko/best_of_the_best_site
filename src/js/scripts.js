07f4bba7591ab72dc4dbe61ee89f7e7abe504050
JeniaSavenko <34649508+JeniaSavenko@users.noreply.github.com>
2018-01-30 18:47:10 +0300

    magistr yoba 17

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -3,29 +3,37 @@ $(document).ready(function () {
 
 
   function mMenu() {
-    var btnMenu = $(".intro-section__menu-btn");
+    var btnMenu = $(".intro-section__menu-btn--hidden-for-pc");
     var menu = $(".intro-section__item--menu");
-    var closeBtn = $(".intro-section__close-btn--hidden-for-pc");
     var menuLogo = $(".intro-section__logo--menu");
     var defLogo = $(".intro-section__logo--default");
+    var headerMenu = $(".intro-section__header");
+    var footerMenu = $(".intro-section__footer");
 
     defLogo.show();
     menuLogo.hide();
 
     if ($(window).width() <= 768) {
-      btnMenu.on("click", function () {
-        menu.show().addClass("collapse");
-        defLogo.hide();
-        menuLogo.show();
+      menu.attr("aria-expanded", "false");
+      btnMenu.on("click tap", function () {
+        if ($(document).find(".collapse").length > 0) {
+          menu.attr("aria-expanded", "false");
+          menu.hide().removeClass("collapse");
+          headerMenu.removeClass("collapse");
+          footerMenu.removeClass("collapse");
+          defLogo.show();
+          menuLogo.hide();
+        }
+        else {
+          menu.attr("aria-expanded", "true");
+          menu.show().addClass("collapse");
+          headerMenu.addClass("collapse");
+          footerMenu.addClass("collapse");
+          defLogo.hide();
+          menuLogo.show();
+        }
       });
-
       menu.hide();
-
-      closeBtn.on("click", function () {
-        menu.hide().removeClass("collapse");
-        defLogo.show();
-        menuLogo.hide();
-      });
     }
   }
 });
